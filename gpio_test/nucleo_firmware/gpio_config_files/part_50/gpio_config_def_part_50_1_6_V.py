5a9405de2d9d49ca6795d042c4fed7adc6eab0c9
IAmMarcelJung <MarcelJung97@web.de>
2025-10-28 10:42:29 +0100

    feat: add more gpio config pattern files

---
+++ gpio_test/nucleo_firmware/gpio_config_files/part_50/gpio_config_def_part_50_1_6_V.py
@@ -0,0 +1,12 @@
+# gpio_config_def.py file for part 50
+# io_config -- version 1.2.2
+part = 50
+voltage = 1.60
+analog = False
+
+H_NONE        = 0
+H_DEPENDENT   = 1
+H_INDEPENDENT = 2
+H_SPECIAL     = 3
+H_UNKNOWN     = 4
+
