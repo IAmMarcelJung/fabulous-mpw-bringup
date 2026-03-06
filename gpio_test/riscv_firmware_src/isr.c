a529dd18a481bf3dad00d75d0705e75ec485ccb9
Marcel Jung <MarcelJung97@web.de>
2026-03-06 16:28:17 +0100

    chore: format and fix includes for the isr and print_io files

--- gpio_test/riscv_firmware_src/isr.c
+++ gpio_test/riscv_firmware_src/isr.c
@@ -1,20 +1,19 @@
 // This file is Copyright (c) 2020 Florent Kermarrec <florent@enjoy-digital.fr>
 // License: BSD
 
-#include <defs.h>
-#include <irq_vex.h>
+#include "isr.h"
 
 uint16_t flag;
 
 void isr(void) {
-    irq_setmask(0);
-    reg_timer0_irq_en = 0; // disable interrupt
+  irq_setmask(0);
+  reg_timer0_irq_en = 0; // disable interrupt
 
-    reg_timer0_update = 1;
-    if (reg_timer0_value == 0)
-        flag = 1;
-    else
-        reg_timer0_irq_en = 1;
+  reg_timer0_update = 1;
+  if (reg_timer0_value == 0)
+    flag = 1;
+  else
+    reg_timer0_irq_en = 1;
 
-    return;
+  return;
 }
