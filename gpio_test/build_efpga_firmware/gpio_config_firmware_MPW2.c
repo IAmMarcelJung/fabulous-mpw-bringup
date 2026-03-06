dacd936e2c71976e7e7b9131eae6c2638f6a55a3
Marcel Jung <MarcelJung97@web.de>
2026-03-06 16:41:30 +0100

    chore: clean up the firmware for MPW2 chips

--- gpio_test/build_efpga_firmware/gpio_config_firmware_MPW2.c
+++ gpio_test/build_efpga_firmware/gpio_config_firmware_MPW2.c
@@ -1,30 +1,29 @@
-#include "../gpio_config/gpio_config_io.h"
-#include <bitstream.h>
 #include <defs.h>
 #include <global_defs.h>
 #include <gpio_config_data.h>
+#include <gpio_config_io.h>
 #include <helpers.h>
 #include <register_actions.h>
 #include <stub.h>
 
 int main() {
-    reg_gpio_mode1 = 1;
-    reg_gpio_mode0 = 0;
-    reg_gpio_ien = 1;
-    reg_gpio_oe = 1;
-    reg_gpio_out = 1;
+  reg_gpio_mode1 = 1;
+  reg_gpio_mode0 = 0;
+  reg_gpio_ien = 1;
+  reg_gpio_oe = 1;
+  reg_gpio_out = 1;
 
-    blink(6, 2500000);
+  blink(6, 2500000);
 
-    // Configure the IOs so the eFPGA has access to them
-    reg_gpio_out = 1;
-    gpio_config_io(config_stream);
+  // Configure the IOs so the eFPGA has access to them
+  reg_gpio_out = 1;
+  gpio_config_io(config_stream);
 
-    blink(3, 2500000);
-    reg_gpio_out = 0;
+  blink(3, 2500000);
+  reg_gpio_out = 0;
 
-    while (1) {
-    }
+  while (1) {
+  }
 
-    return 0;
+  return 0;
 }
